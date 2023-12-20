d0cdd7bf247d8d2afbefa2344d1fe6791cef50ef
aissahoney <aissatou.onipogui@gmail.com>
2023-12-20 21:20:12 +0100

    update

--- public/js/main.js
+++ public/js/main.js
@@ -10,6 +10,112 @@ clickDrop.addEventListener('click', () => {
     console.log(dropItems)
 })
 
+// color change section servive
+
+let gridAll = document.querySelectorAll(".grid-item")
+let gridItems;
+// console.log(gridItems)
+// let i = document.querySelector(".fa-music")
+// console.log(i);
+gridAll.forEach(element => {
+    gridItems = element
+    console.log(gridItems)
+    element.addEventListener("mouseover", function (e) {
+        element = e.target
+        console.log(element.children)
+        if (element.classList.contains("item1")) {
+            // i.style.color= "#fff"
+            element.children[0].style.color = "white"
+            element.children[1].style.color = "white"
+            element.children[2].style.color = "white"
+            element.style.background = "#47AEFE"
+        }
+        if (element.classList.contains("item2")) {
+            element.children[0].style.color = "white"
+            element.children[1].style.color = "white"
+            element.children[2].style.color = "white"
+            element.style.background = "rgb(255,162 ,106 )"
+        }
+        if (element.classList.contains("item3")) {
+            element.children[0].style.color = "white"
+            element.children[1].style.color = "white"
+            element.children[2].style.color = "white"
+            element.style.background = "#E80168"
+        }
+        if (element.classList.contains("item4")) {
+            element.children[0].style.color = "white"
+            element.children[1].style.color = "white"
+            element.children[2].style.color = "white"
+            element.style.background = "#FFBB2C"
+        }
+        if (element.classList.contains("item5")) {
+            element.children[0].style.color = "white"
+            element.children[1].style.color = "white"
+            element.children[2].style.color = "white"
+            element.style.background = "rgb(250,77 ,36 )"
+        }
+        if (element.classList.contains("item6")) {
+            element.children[0].style.color = "white"
+            element.children[1].style.color = "white"
+            element.children[2].style.color = "white"
+            element.style.background = "#0EDBCE"
+        }
+
+    })
+});
+gridAll.forEach(element => {
+    gridItems = element
+    console.log(gridItems)
+    element.addEventListener("mouseout", function (e) {
+        element = e.target
+        console.log(element.children)
+        if (element.classList.contains("item1")) {
+            element.children[0].style.color = "#47AEFE"
+            element.children[1].style.color = "black"
+            element.children[2].style.color = "black"
+            element.style.background = "white"
+        }
+        if (element.classList.contains("item2")) {
+            element.children[0].style.color = "rgb(255,162 ,106 )"
+            element.children[1].style.color = "black"
+            element.children[2].style.color = "black"
+            element.style.background = "white"
+        }
+        if (element.classList.contains("item3")) {
+            element.children[0].style.color = "#E80168"
+            element.children[1].style.color = "black"
+            element.children[2].style.color = "black"
+            element.style.background = "white"
+        }
+        if (element.classList.contains("item4")) {
+            element.children[0].style.color = "#FFBB2C"
+            element.children[1].style.color = "black"
+            element.children[2].style.color = "black"
+            element.style.background = "white"
+        }
+        if (element.classList.contains("item5")) {
+            element.children[0].style.color = "rgb(250,77 ,36 )"
+            element.children[1].style.color = "black"
+            element.children[2].style.color = "black"
+            element.style.background = "white"
+        }
+        if (element.classList.contains("item6")) {
+            element.children[0].style.color = "#0EDBCE"
+            element.children[1].style.color = "black"
+            element.children[2].style.color = "black"
+            element.style.background = "white"
+        }
+    })
+});
+
+
+
+
+
+
+
+
+
 
 
 
@@ -28,10 +134,10 @@ console.log(uxButton);
 
 
 
-let containerDiv= document.querySelector('.container')
+let containerDiv = document.querySelector('.container')
 console.log(containerDiv)
 
-let filterDiv= containerDiv.children
+let filterDiv = containerDiv.children
 console.log(filterDiv)
 
 
@@ -39,10 +145,10 @@ aiButton.addEventListener('click', function () {
     Array.from(filterDiv).forEach(div => {
         if (div.classList.contains('ia')) {
             div.classList.add("filtered-div")
-            div.style.display="block"
+            div.style.display = "block"
         }
-        else if (!div.classList.contains('ia')){
-            div.style.display="none"
+        else if (!div.classList.contains('ia')) {
+            div.style.display = "none"
             console.log("remove dev et ux");
         }
     })
@@ -51,23 +157,23 @@ devButton.addEventListener('click', function () {
     Array.from(filterDiv).forEach(div => {
         if (div.classList.contains('dev')) {
             div.classList.add("filtered-div")
-            div.style.display="block"
+            div.style.display = "block"
         }
-        else if (!div.classList.contains('dev')){
-            div.style.display="none"
+        else if (!div.classList.contains('dev')) {
+            div.style.display = "none"
             console.log("remove ai et ux");
         }
     })
 })
 
-uxButton.addEventListener('click', function() {
+uxButton.addEventListener('click', function () {
     Array.from(filterDiv).forEach(div => {
         if (div.classList.contains('ux')) {
             div.classList.add("filtered-div")
-            div.style.display="block"
+            div.style.display = "block"
         }
-        else if (!div.classList.contains('ux')){
-            div.style.display="none"
+        else if (!div.classList.contains('ux')) {
+            div.style.display = "none"
             console.log("remove ai et dev");
         }
     })
@@ -80,25 +186,27 @@ uxButton.addEventListener('click', function() {
 //accordion section faqs
 let accordeonTitles = document.querySelectorAll('.accordeonTitle')
 // console.log(accordeonTitles);
-let arrowDown= document.querySelectorAll(".fa-chevron-down")
+let arrowDown = document.querySelectorAll(".fa-chevron-down")
 // console.log(arrowDown)
-let arrowUp=document.createElement("i")
-arrowUp.classList.add("fa-solid")
-arrowUp.classList.add("fa-chevron-up")
+let arrowUp = document.querySelector(".fa-chevron-up")
+console.log(arrowUp)
+
 
 
 accordeonTitles.forEach(element => {
-    element.addEventListener('click',function(e){
-        // element.nextElementSibling.style.display = 'block'
+    element.addEventListener('click', function () {
         element.nextElementSibling.className = 'block'
-        arrowDown.forEach(element => {
-            element=e.target
-            element.replaceWith(arrowUp)
-        });
+        // arrowDown.forEach(item=> {
+        //     item.replaceWith(arrowUp)
+        //     // item.style.display="block"
+        //     arrowUp.style.display="block"
+        //     console.log(item)
+        // });
 
         accordeonTitles.forEach(item => {
             if (element != item) {
-                // item.nextElementSibling.style.display = 'none'
+                // console.log(item)
+                // item.replaceWith(arrowDown)
                 item.nextElementSibling.className = 'none'
             }
         })
