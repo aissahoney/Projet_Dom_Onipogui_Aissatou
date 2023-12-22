971b188cf1840cd53223b16f96e6dd72c096e011
aissahoney <aissatou.onipogui@gmail.com>
2023-12-22 12:28:31 +0100

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,78 @@
 
 // nav
 let clickDrop = document.querySelector(".dropdown")
-console.log(clickDrop)
+// console.log(clickDrop)
 let dropItems = document.querySelector(".close")
 console.log(dropItems)
 
 clickDrop.addEventListener('click', () => {
     dropItems.classList.toggle('open')
     dropItems.classList.toggle('close')
-    console.log(dropItems)
+    // console.log(dropItems)
 })
+
+// menu-burger
+let menuburger = document.querySelector(".menu-burger")
+let navLinks = document.querySelector(".nav-links")
+
+menuburger.addEventListener('click',()=>{
+navLinks.classList.toggle('mobile-menu')
+})
+
+//light/Dark
+let body= document.querySelector("body")
+let section= document.querySelectorAll('section')
+let h1=document.querySelectorAll("h1")
+let h2=document.querySelectorAll("h2")
+let h3=document.querySelectorAll("h3")
+let h4=document.querySelectorAll("h4")
+let h5=document.querySelectorAll("h5")
+let p=document.querySelectorAll("p")
+let ul=document.querySelectorAll("ul")
+let nav=document.querySelector(".navbar")
+
+
+
+let lightMode=document.querySelector(".mode")
+lightMode.addEventListener("click", function() {
+    lightMode.classList.toggle(".mode")
+    
+    body.style.background=" rgba(19, 18, 29, 0.9)"
+    body.style.color="white"
+    nav.style.color="rgba(87, 58, 250, 0.966)"
+
+    section.forEach(element => {
+        element.style.background=" rgba(19, 18, 29, 0.9)"
+        
+    });
+    h1.forEach(element => {
+        element.style.color="white"
+    });
+    h2.forEach(element => {
+        element.style.color="white"
+    });
+    h3.forEach(element => {
+        element.style.color="white"
+    });
+    h4.forEach(element => {
+        element.style.color="white"
+    });
+    h5.forEach(element => {
+        element.style.color="white"
+    });
+    
+    p.forEach(element => {
+        element.style.white="white"
+    });
+    li.forEach(element => {
+        element.style.white="white"
+    });
+
+
+})
+
+
+
 
 // color change section servive
 
@@ -17,10 +80,11 @@ let gridAll = document.querySelectorAll(".grid-item")
 let gridItems;
 // console.log(gridItems)
 // let i = document.querySelector(".fa-music")
+// i.style.color = 'red'
 // console.log(i);
 gridAll.forEach(element => {
     gridItems = element
-    console.log(gridItems)
+    // console.log(gridItems)
     element.addEventListener("mouseover", function (e) {
         element = e.target
         console.log(element.children)
@@ -66,7 +130,7 @@ gridAll.forEach(element => {
 });
 gridAll.forEach(element => {
     gridItems = element
-    console.log(gridItems)
+    // console.log(gridItems)
     element.addEventListener("mouseout", function (e) {
         element = e.target
         console.log(element.children)
@@ -126,13 +190,11 @@ gridAll.forEach(element => {
 // tabs filter section current
 
 let aiButton = document.querySelector('.ai-btn')
-console.log(aiButton);
+// console.log(aiButton);
 let devButton = document.querySelector('.dev-btn')
-console.log(devButton);
+// console.log(devButton);
 let uxButton = document.querySelector('.ux-btn')
-console.log(uxButton);
-
-
+// console.log(uxButton);
 
 
 let containerDiv = document.querySelector('.container')
@@ -262,6 +324,13 @@ function slide(wrapper, items, prev, next) {
 
     items.insertBefore(slides[slidesLength - 1].cloneNode(true), slides[0]);
 
+    // Mouse events
+    //   items.onmousedown = dragStart;
+
+    //   // Touch events
+    //   items.addEventListener('touchstart', dragStart);
+    //   items.addEventListener('touchend', dragEnd);
+    //   items.addEventListener('touchmove', dragAction);
 
     // Click events
     prev.addEventListener('click', function () { shiftSlide(-1) });
@@ -269,47 +338,6 @@ function slide(wrapper, items, prev, next) {
 
     // Transition events
     items.addEventListener('transitionend', checkIndex);
-
-    function dragStart(e) {
-        e = e || window.Event;
-        e.preventDefault();
-        posInitial = items.offsetLeft;
-
-        if (e.type == 'touchstart') {
-            posX1 = e.touches[0].clientX;
-        } else {
-            posX1 = e.clientX;
-            document.onmouseup = dragEnd;
-            document.onmousemove = dragAction;
-        }
-    }
-
-    function dragAction(e) {
-        e = e || window.Event;
-
-        if (e.type == 'touchmove') {
-            posX2 = posX1 - e.touches[0].clientX;
-            posX1 = e.touches[0].clientX;
-        } else {
-            posX2 = posX1 - e.clientX;
-            posX1 = e.clientX;
-        }
-        items.style.left = (items.offsetLeft - posX2) + "px";
-    }
-
-    function dragEnd() {
-        posFinal = items.offsetLeft;
-        if (posFinal - posInitial < -threshold) {
-            shiftSlide(1, 'drag');
-        } else if (posFinal - posInitial > threshold) {
-            shiftSlide(-1, 'drag');
-        } else {
-            items.style.left = (posInitial) + "px";
-        }
-
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
 
     function shiftSlide(dir, action) {
         items.classList.add('shifting');
@@ -369,3 +397,49 @@ function slide(wrapper, items, prev, next) {
 }
 
 slide(slider, sliderItems, prev, next);
+
+
+
+let btnMonth= document.querySelector(".btn-change"); 
+let grandRond = document.querySelectorAll(".grand-rond")[0]; 
+let boule = document.querySelectorAll(".boule") [0];
+let prix1= document.querySelectorAll(".price")[0];
+let prix2= document.querySelectorAll(".price")[1];
+let prix3= document.querySelectorAll(".price")[2];
+let prix4= document.querySelectorAll(".price")[3];
+let month1= document.querySelectorAll(".month")[0];
+let month2= document.querySelectorAll(".month")[1];
+let month3= document.querySelectorAll(".month")[2];
+let month4= document.querySelectorAll(".month")[3];
+console.log(prix1);
+console.log(month1);
+
+let month = true
+
+grandRond.addEventListener("click",function(){
+    if(month){
+        boule.style.left="17.1rem"
+        month1.innerHTML = "/year"
+        month2.innerHTML = "/year"
+        month3.innerHTML = "/year"
+        month4.innerHTML = "/year"
+    
+        prix2.innerHTML="190"
+        prix3.innerHTML="290"
+        prix4.innerHTML="490"
+        month = false
+
+    } else{
+        month = true
+        boule.style.left="13rem"
+        month1.innerHTML = "/month"
+        month2.innerHTML = "/month"
+        month3.innerHTML = "/month"
+        month4.innerHTML = "/month"
+    
+        prix2.innerHTML="19"
+        prix3.innerHTML="29"
+        prix4.innerHTML="49"
+
+    }
+})
